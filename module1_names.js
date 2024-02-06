ddd9557594d0b310857a8b62f30efb9459c77451
RUKUNDO Princde <rukundoprince951@gmail.com>
2024-02-06 07:45:46 +0200

    initialised the project

--- module1_names.js
+++
@@ -1,5 +0,0 @@
-const secret = "Super secret";
-const jodos = "Jodos";
-const lucky = "Lucky";
-
-module.exports = {jodos, lucky}
--- module3_function2.js
+++
@@ -1,7 +0,0 @@
-num1 = 2;
-num2 = 3;
-
-const functionAdd = () => {
-    console.log(`The sum is ${num1 + num2}`);
-}
-functionAdd();
